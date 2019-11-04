045f41257543612f04e3e42dfb583271b3e53a00
bentrantanella <57221780+bentrantanella@users.noreply.github.com>
2019-11-03 20:31:56 -0500

    Update ParserGUI.java

--- Parser Program/src/parser/ParserGUI.java
+++ Parser Program/src/parser/ParserGUI.java
@@ -25,26 +25,35 @@ public class ParserGUI extends GBFrame {
 	public void buttonClicked(JButton buttonObj) {
 		if (buttonObj == calculateButton) {
 			input = inputField.getText();
-			
 			char[] expression = new char[input.length()];
-			
 			expression = input.toCharArray();
 			
-			int spcounter = parser.findSpaces(expression);
-
-			char[] newexpression = new char[spcounter];
-			
-			newexpression = parser.removeSpaces(expression, spcounter);
-			
-			for(int x = 0; x < newexpression.length; x++)
-				output.concat(newexpression[x] + " ");
-			
-			outputLabel.setText(output);
-			revalidate();
+			if (expression[0] != '=')
+				messageBox("Input must begn with an equals sign");
+			else
+			{
+				int spcounter = parser.findSpaces(expression);
+				char[] newexpression = new char[spcounter];
+				newexpression = parser.removeSpaces(expression, spcounter);
+				
+				double num1 = parser.findNum1(newexpression);
+				System.out.println(num1);
+				String type = parser.findOperator(newexpression);
+				
+				if (type == "error")
+					messageBox("Invalid input");
+				
+				System.out.println(type);
+				outputLabel.setText(output);
+				revalidate();
+			}
 		}
 		
 		if (buttonObj == clearButton) {
-			
+			inputField.setText("");
+			outputLabel.setText("");
+			output = "";
+			input = "";
 		}
 		
 		if (buttonObj == exitButton) {
